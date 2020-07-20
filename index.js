c9c8af4856cdef204e51c6e5782449dba55dac58
Milan Winter <milanwinter8@gmail.com>
2020-07-20 20:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+let fusion = []// add solution here
+function theBeatlesPlay(musicians,instruments) {
+  for (let i = 0; i < 4; i++) {
+    fusion.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return fusion;
+}
+let facts = ["He was the last Beatle to learn to drive",
+            "He was never a vegetarian",
+            "He was a choir boy and boy scout",
+            "He hated the sound of his own voice"]
+
+function johnLennonFacts (facts) {
+  let i = 0
+  while (i<facts.length) {
+  facts.push("${facts[i]}!!!")
+  }
+  return facts;
+}
